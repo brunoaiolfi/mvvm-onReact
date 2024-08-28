e35ac3ef31520e6f6bf741ec957a605c685e7354
bruno.aiolfi154@gmail.com <you@example.com>
2024-08-28 15:10:56 -0300

    feat applyErrorHandler

--- src/infra/api/index.ts
+++ src/infra/api/index.ts
@@ -1,5 +1,19 @@
-import axios from "axios";
+import axios, { Axios } from "axios";
 
 export const api = axios.create({
     baseURL: ""
 })
+
+applyErrorHandler(api);
+
+function applyErrorHandler(api: Axios) {
+    api.interceptors.response.use(response => {
+        if (response.data.success === false) {
+            return Promise.reject(new Error(response.data.message));
+        }
+
+        return response
+    }, error => {
+        return Promise.reject(new Error(error));
+    })
+}
